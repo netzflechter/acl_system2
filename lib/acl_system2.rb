963645d26d5e5ffade10cc1cf6f28a718de6928e
Gareth Rees <gareth.rees@boxuk.com>
2013-01-29 16:26:03 +0000

    Reorganise in to gem structure

---
+++ lib/acl_system2.rb
@@ -0,0 +1,4 @@
+require 'acl_system2/caboose/logic_parser'
+require 'acl_system2/caboose/role_handler'
+require 'acl_system2/caboose/access_control'
+
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,5 @@ Turn.config do |c|
  c.format  = :dot
 end
 
-require File.dirname(__FILE__) + '/../lib/caboose/logic_parser'
-require File.dirname(__FILE__) + '/../lib/caboose/role_handler'
-require File.dirname(__FILE__) + '/../lib/caboose/access_control'
+require "#{ File.dirname(__FILE__) }/../lib/acl_system2"
+
