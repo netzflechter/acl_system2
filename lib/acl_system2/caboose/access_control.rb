bdc29ecdb75e3a34f256b0a458ea0b5f1593d08c
Gareth Rees <gareth.rees@boxuk.com>
2013-03-06 16:06:24 +0000

    Abstract AccessSentry class

--- lib/acl_system2/caboose/access_control.rb
+++ lib/acl_system2/caboose/access_control.rb
@@ -1,3 +1,4 @@
+require "#{ File.dirname(__FILE__) }/access_control/access_sentry"
 
 module Caboose
 
@@ -79,34 +80,7 @@ module Caboose
       end 
       result
     end    
-      
-    class AccessSentry
-     
-      def initialize(subject, actions={})
-        @actions = actions.inject({}) do |auth, current|
-          [current.first].flatten.each { |action| auth[action] = current.last }
-          auth
-        end 
-        @subject = subject
-      end 
-     
-      def allowed?(action)
-        if @actions.has_key? action.to_sym
-          return @subject.access_handler.process(@actions[action.to_sym].dup, @subject.access_context)
-        elsif @actions.has_key? :DEFAULT
-          return @subject.access_handler.process(@actions[:DEFAULT].dup, @subject.access_context) 
-        else
-          return true
-        end  
-      end
-   
-    end # AccessSentry
-  
-  end # AccessControl  
-  
-end # Caboose    
 
+  end # AccessControl
 
-
-
-
+end # Caboose
